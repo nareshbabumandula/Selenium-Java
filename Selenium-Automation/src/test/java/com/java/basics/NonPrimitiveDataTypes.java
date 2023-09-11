b98f6e65ca8ee8bb64181ca5ec8ead4d3b3a81b1
nareshbabumandula <naresh223@gmail.com>
2023-09-11 09:19:35 +0530

    Added integer array example

--- Selenium-Automation/src/test/java/com/java/basics/NonPrimitiveDataTypes.java
+++ Selenium-Automation/src/test/java/com/java/basics/NonPrimitiveDataTypes.java
@@ -11,7 +11,15 @@ public class NonPrimitiveDataTypes {
 		System.out.println(names[0]);
 		System.out.println(names[1]);
 		System.out.println(names[2]);
-		System.out.println(names[5]); // ArrayIndexOutOfBoundsException
+		//System.out.println(names[5]); // ArrayIndexOutOfBoundsException
+		
+		int[] marks = {10,60,50,80,90};
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[3]);
+		
+		
 
 	}
 
