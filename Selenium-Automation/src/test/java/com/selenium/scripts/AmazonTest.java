f5b388af3b91aeffb791fb4f9852d6c162d97ac3
nareshbabumandula <naresh223@gmail.com>
2023-10-12 10:01:49 +0530

    12-Oct-2023: Naresh: testNG groups, before after method, dependsonmethods,alwaysRun and enabled

--- Selenium-Automation/src/test/java/com/selenium/scripts/AmazonTest.java
+++ Selenium-Automation/src/test/java/com/selenium/scripts/AmazonTest.java
@@ -1,40 +1,49 @@
 package com.selenium.scripts;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.runner.Base;
 
 public class AmazonTest extends Base{
 
-	@Test(priority = 7)
-	public void proceedtoBuy() {
-		System.out.println("Executing Proceed to Buy method");
-	}
 		
-	@Test(priority = 4)
+	@Test(priority = 1, groups = {"smoke"})
 	public void searchProduct() {
 		System.out.println("Executing Search Product method");
 	}
 	
-	@Test(priority = 5)
+	@Test(priority = 2, groups = {"smoke"} )
 	public void addToCart() {
 		System.out.println("Executing Add to Cart method");
 	}
 	
-	@Test(priority = 6)
+	@Test(priority = 3)
 	public void checkout() {
 		System.out.println("Executing Checkout method");
 	}
 		
-	@Test(priority = 8)
+	@Test(priority = 4)
+	public void proceedtoBuy() {
+		System.out.println("Executing Proceed to Buy method");
+	}
+	
+	@Test(dependsOnMethods = {"proceedtoBuy"})
 	public void selectDeliveryAddress() {
+		Assert.assertTrue(false,"Default delivery address is selected");
 		System.out.println("Executing Select Delivery Address method");
 	}
 	
-	@Test(priority = 9)
-	public void placeOrder() {
+	@Test(dependsOnMethods = {"selectDeliveryAddress"})
+	public void orderProduct() {
 		System.out.println("Executing Place Order method");
 	}
+	
+	@Test(dependsOnMethods = {"addToCart"}, enabled = true)
+	public void verifyProductRating() {
+		System.out.println("Executing verify Product Rating method");
+	}
+	
 	
 	
 		
--- Selenium-Automation/src/test/java/com/selenium/scripts/Common.java
+++ Selenium-Automation/src/test/java/com/selenium/scripts/Common.java
@@ -1,18 +1,20 @@
 package com.selenium.scripts;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 public class Common {
 	
-	@BeforeTest
+	@BeforeTest(groups = {"smoke"})
 	public void accessAmazonPortal() {
 		System.out.println("Executing access Amazon portal method");
 	}
 	
-	@BeforeClass
+	@BeforeClass(groups = {"smoke"})
 	public void login() {
 		System.out.println("Executing Login method");
 	}
@@ -26,5 +28,19 @@ public class Common {
 	public void logout() {
 		System.out.println("Executing Logout method");
 	}
+	
+	
+	@BeforeMethod()
+	public void verifyProduct() {
+		System.out.println("Executing verify product method");
+	}
+	
+	
+	@AfterMethod
+	public void verifyQuantity() {
+		System.out.println("Executing verify quantity method");
+	}
+	
+	
 		
 }
